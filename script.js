bb3f9b0319d0dab83c4efeabda21338baf6bf5ed
Vlad-Os1 <oshovskiyvlad28@gmail.com>
2024-04-23 17:25:31 +0300

    feat: add color selection and change painting type
    The grid now allows users to select colors and uses a new painting type

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
-let grid = document.querySelector("#sketch-container");
-let sizeBtn = document.querySelector("#size-btn");
-let resetBtn = document.querySelector("#reset-btn");
+const grid = document.querySelector("#sketch-container");
+const sizeBtn = document.querySelector("#size-btn");
+const resetBtn = document.querySelector("#reset-btn");
+const changeColorBtn = document.querySelector("#change-color-btn")
+const rgbBtn = document.querySelector("#rgb-btn")
+// let isMouseDown = false; 
 
 let size = 16;
+let color = "grey";
+
 
 resetBtn.addEventListener ("click", (e) => {
   redrawGrid();
@@ -35,19 +40,38 @@ function redrawGrid() {
 }
 
 
-grid.addEventListener ("mouseover", (e) => {
-  if (e.target.classList.contains("gridElement")){
-    if (size < 10){  //deletes hover on too small grids 
+function getColor(newColor) {
+  do {
+    newColor = prompt("What color do you want to choose?");
+  } while (!isValidColor(newColor));
+  return newColor;
+}
+
+function isValidColor(color) {
+  let div = document.createElement("div");
+  div.style.backgroundColor = color;
+  
+  return div.style.backgroundColor !== "";
+};
+
+changeColorBtn.addEventListener ("click", () => {
+  color = getColor(color);
+});
+
+
+
+let isDrawing = false; 
+
+grid.addEventListener("mousedown", (e) => {
+  isDrawing = !isDrawing; 
+});
+
+grid.addEventListener ("mousemove", (e) => {
+  if (isDrawing && e.target.classList.contains("gridElement")){
       e.target.classList.add("noHover");
-      e.target.style.backgroundColor = "grey";  
-    } else { 
-      e.target.style.backgroundColor = "grey";
-    }
+      e.target.style.backgroundColor = color;
   }
 })
-
-
-
 
 
 
